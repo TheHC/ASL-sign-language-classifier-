ee887cf2a963f04a03a1263acc01396e7ab11a8d
Hamza CHETTOUR <hamzachettour@gmail.com>
2018-09-03 19:36:17 +0000

    feat: Frozen graph to UFF script

---
+++ Frozen_graph_to_UFF.py
@@ -0,0 +1,17 @@
+import os
+import uff
+
+
+FROZEN_GRAPH_FILENAME='Model/model0/frozen_model.pb'
+OUTPUT_NAME='logits/BiasAdd'
+OUTPUT_UFF_FILE_NAME='Inference_engine/UFF.uff'
+
+def UFF_write():
+	uff.from_tensorflow_frozen_model(
+		frozen_file=FROZEN_GRAPH_FILENAME,
+		output_nodes=[OUTPUT_NAME],
+		output_filename=OUTPUT_UFF_FILE_NAME,
+		text=False,
+		)
+if __name__ == '__main__' : 
+	UFF_write()
